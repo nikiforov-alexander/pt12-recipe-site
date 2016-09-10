7b42d0cb65dc1585ecb3199e74c702926aa93828
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-09-10 21:12:38 +0200

    added getters and setters to User class as well as override methods and constructors, not it just like in spring-unit-test-weather app

--- src/main/java/com/techdegree/model/User.java
+++ src/main/java/com/techdegree/model/User.java
@@ -3,23 +3,31 @@ package com.techdegree.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
+import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Collection;
 
-// extends BaseEntity, so that no @Id and @Version fields
+// does not extend BaseEntity, so that no @Id and @Version fields
+// added automatically, so I'll put @Id
 // here, and implements UserDetails, well because I also
 // take @christherama's position on this
 @Entity
-public class User extends BaseEntity implements UserDetails {
+public class User implements UserDetails {
+    public static final PasswordEncoder PASSWORD_ENCODER =
+            new BCryptPasswordEncoder();
 
     // fields
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     // let it be just "name" for now. It could be split into
     // first name and last name, but well, for now I'll leave it
     // be so
@@ -32,47 +40,113 @@ public class User extends BaseEntity implements UserDetails {
             message = "Username should be between 2 and 20 characters")
     private String username;
 
+    // no constraints here... that is a good question to
+    // @christherama
     @JsonIgnore
     private String password;
 
+    // adding this because of 'spring-weather-app'
+    // by @christherama ... should ask later
+    @Column(nullable = false)
+    private boolean enabled;
+
+    // role: for now I'll leave it one role, not like
+    // @craigsdennis made them String[] ... but again
+    // I better ask this...
+    @OneToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
     // Getters and Setters
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = PASSWORD_ENCODER.encode(password);
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
     // Constructors
+
+    public User() {
+        this.id = null;
+        this.username = null;
+        this.password = null;
+        this.enabled = true;
+        this.role = null;
+    }
+
+    public User(String username, String password) {
+        this();
+        this.username = username;
+        setPassword(password);
+    }
 
     // Override methods
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return AuthorityUtils.createAuthorityList(
+                role.getName()
+        );
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return username;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return enabled;
     }
 }
