19b0ee47497964a5275451edcdcd8e0402073551
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-09-11 16:38:35 +0200

    added all of neccessary fields for Recipe class, getters and setters and couple of constructors. Added couple of method adding steps an ingredients

--- src/main/java/com/techdegree/model/Recipe.java
+++ src/main/java/com/techdegree/model/Recipe.java
@@ -1,7 +1,156 @@
 package com.techdegree.model;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
 import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Recipe extends BaseEntity{
+
+    // fields
+
+    @NotNull
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    @NotNull
+    private String description;
+
+    @NotEmpty
+    @NotNull
+    @Enumerated
+    private RecipeCategory recipeCategory;
+
+    // for now we'll make it photo url, later
+    // hopefully user can upload picture
+    @NotEmpty
+    @NotNull
+    private String photoUrl;
+
+    @NotNull
+    @NotEmpty
+    private String preparationTime;
+
+    @NotNull
+    @NotEmpty
+    private String cookTime;
+
+    @NotEmpty
+    @NotNull
+    @OneToMany
+    private List<Ingredient> ingredients = new ArrayList<>();
+
+    @NotEmpty
+    @NotNull
+    @OneToMany
+    private List<Step> steps = new ArrayList<>();
+
+    // getters and setters
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public RecipeCategory getRecipeCategory() {
+        return recipeCategory;
+    }
+
+    public void setRecipeCategory(RecipeCategory recipeCategory) {
+        this.recipeCategory = recipeCategory;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getPreparationTime() {
+        return preparationTime;
+    }
+
+    public void setPreparationTime(String preparationTime) {
+        this.preparationTime = preparationTime;
+    }
+
+    public String getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(String cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<Step> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(List<Step> steps) {
+        this.steps = steps;
+    }
+
+    // constructors
+
+    // default constructor for JPA, with parent
+    // constructor inside
+    public Recipe() {
+        super();
+    }
+
+    // constructor to be used in DataLoader for easier
+    // data injection and testing
+    public Recipe(
+            String name,
+            String description,
+            RecipeCategory recipeCategory,
+            String photoUrl,
+            String preparationTime,
+            String cookTime) {
+        this();
+        this.name = name;
+        this.description = description;
+        this.recipeCategory = recipeCategory;
+        this.photoUrl = photoUrl;
+        this.preparationTime = preparationTime;
+        this.cookTime = cookTime;
+    }
+
+    // additional methods adding Ingredients or Steps
+
+    public void addIngredient(Ingredient ingredient) {
+        ingredients.add(ingredient);
+    }
+
+    public void addStep(Step step) {
+        steps.add(step);
+    }
 }
