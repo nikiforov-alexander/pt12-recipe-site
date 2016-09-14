8b4921c43171563d1a58fb1827d1d6dd335aac35
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-09-14 19:44:25 +0200

    changed DataLoader so that @OneToMany Recipe->Steps, and Recipe->ingredients schema works... Nice. Added manual save of ingredients and steps, otherwise they are not saved when recipe saved.

--- src/main/java/com/techdegree/DataLoader.java
+++ src/main/java/com/techdegree/DataLoader.java
@@ -48,12 +48,9 @@ public class DataLoader implements ApplicationRunner {
         Ingredient ingredient2 =
                 new Ingredient(item2, "condition 2", "quantity 2");
 
-        // create step and save,
-        // because they are in @ManyToMany now
+        // create steps
         Step step1 = new Step("step 1");
         Step step2 = new Step("step 2");
-        stepDao.save(step1);
-        stepDao.save(step2);
 
         // create recipe
         Recipe recipe = new Recipe(
@@ -69,8 +66,8 @@ public class DataLoader implements ApplicationRunner {
         recipe.addIngredient(ingredient2);
         // put steps into recipe from
         // already existing steps
-        recipe.addStep(stepDao.findOne(1L));
-        recipe.addStep(stepDao.findOne(1L));
+        recipe.addStep(step1);
+        recipe.addStep(step2);
 
         // set ingredient1-2.recipes to recipe
         ingredient1.setRecipe(recipe);
@@ -78,5 +75,12 @@ public class DataLoader implements ApplicationRunner {
 
         // save recipe
         recipeDao.save(recipe);
+
+        // save ingredients and steps, otherwise they are not
+        // saved with recipes
+        ingredientDao.save(ingredient1);
+        ingredientDao.save(ingredient2);
+        stepDao.save(step1);
+        stepDao.save(step2);
     }
 }
