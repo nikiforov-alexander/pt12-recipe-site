e718753a9d06543089073365da213f0e59e84f36
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-09-16 21:19:00 +0200

    added @Service annotation to ItemService class

--- src/main/java/com/techdegree/service/ItemServiceImpl.java
+++ src/main/java/com/techdegree/service/ItemServiceImpl.java
@@ -3,9 +3,11 @@ package com.techdegree.service;
 import com.techdegree.dao.ItemDao;
 import com.techdegree.model.Item;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ItemServiceImpl implements ItemService {
     @Autowired
     private ItemDao itemDao;
