0538a40994817ea4decf9d768437dc26c1755ab4
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-09-21 20:09:54 +0200

    added function adding new row in `app.js` using JavaScript

--- src/main/resources/static/assets/js/app.js
+++ src/main/resources/static/assets/js/app.js
@@ -7,3 +7,49 @@ $("select").change(function(){
     $(this).removeClass($(this).attr('class'))
         .addClass($(":selected",this).attr('class'));
 });
+
+// function adding new step
+$("#add-another-step-button").click(function () {
+    // can add prevent default so that
+    // no need to write button type=button
+    // in html5
+    event.preventDefault();
+
+    // define <div> with step that we will be appending to
+    var divWithLastStep = $(".step-row").last();
+
+    // get id of the last step to generate new one
+    // it is kind of whacky .. but well I'm JS newbie, so
+    var id = parseInt(
+        divWithLastStep
+            .children('div')
+            .children('p')
+            .children('input')
+            .attr('id')
+            .split('.')[0]
+            .split('steps')[1]
+    );
+
+    // set new id
+    var newId = id + 1;
+    var newStepInputId = 'steps' + newId + '.description';
+    var newStepInputName = 'steps[' + newId + '].description';
+
+    // create new div
+    var newDiv =
+        "<div class='step-row'>" +
+            "<div class='prefix-20 grid-80'>" +
+                "<p>" +
+                    "<input id='" + newStepInputId + "' " +
+                    "name='" + newStepInputName + "' " +
+                    "placeholder='new step'>" +
+                "</p>" +
+            "</div>" +
+        "</div>";
+
+    // add newly created div after last one
+    divWithLastStep.after(
+        newDiv
+    );
+
+});
