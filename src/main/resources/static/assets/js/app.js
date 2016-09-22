bde201d48f39c498e3d50b289f26e3822d4725eb
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-09-22 21:00:09 +0200

    added javaScript code that successfully adds new <div> with ingredient row, right now it is visually tested when recipe is edited.

--- src/main/resources/static/assets/js/app.js
+++ src/main/resources/static/assets/js/app.js
@@ -9,6 +9,8 @@ $("select").change(function(){
 });
 
 // function adding new step
+// for now this script does not take into consideration
+// adding step when there are none
 $("#add-another-step-button").click(function () {
     // can add prevent default so that
     // no need to write button type=button
@@ -30,6 +32,8 @@ $("#add-another-step-button").click(function () {
             .split('steps')[1]
     );
 
+    // TODO: add Nan check here
+
     // set new id
     var newId = id + 1;
     var newStepInputId = 'steps' + newId + '.description';
@@ -50,6 +54,159 @@ $("#add-another-step-button").click(function () {
     // add newly created div after last one
     divWithLastStep.after(
         newDiv
+    );
+
+});
+
+
+/**
+ * changes select item element inside first div
+ * of ingredientRowDiv:
+ * - changes id,name
+ * - removes "selected" attribute from <option>-s inside select
+ * @param ingredientRowDiv - link to ingredientRowDiv with everything
+ * related to one ingredient
+ * @param newIngredientId - id in the array of recipe.ingredients
+ * of the new ingredient to be added
+ */
+function changeSelectItemElement(
+    ingredientRowDiv,
+    newIngredientId) {
+
+    var selectItemElement =
+        ingredientRowDiv
+        .children('div').eq(0)
+        .children('p')
+        .children();
+
+    // change select id
+    selectItemElement.attr(
+            'id',
+            'ingredients' + newIngredientId + '.item.id'
+    );
+
+    // change select name
+    selectItemElement.attr(
+            'name',
+            'ingredients[' + newIngredientId + '].item.id'
+    );
+
+    // remove attribute selected from option
+    selectItemElement
+        .children() // option
+        .removeAttr('selected');
+
+}
+
+/**
+ * changes <input> with ingredientPropertyName
+ * element inside of ingredientRowDiv, inside of
+ * div with indexOfDivInIngredientRow:
+ * <div> - ingredientRowDiv
+ *    <div></div> - div #1
+ *    ...
+ *    <div> - div #indexOfDivInIngredientRow
+ *       <p>
+ *          <input> - input with ingredientPropertyName
+ *       </p>
+ *    </div>
+ * <div>
+ * @param ingredientRowDiv
+ * @param newIdOfIngredient
+ * @param indexOfDivInIngredientRow
+ * @param ingredientPropertyName
+ */
+function changeIngredientInputElement(
+    ingredientRowDiv,
+    newIdOfIngredient,
+    indexOfDivInIngredientRow,
+    ingredientPropertyName) {
+
+    var inputElementToChange =
+        ingredientRowDiv
+            .children('div').eq(indexOfDivInIngredientRow)
+            .children('p')
+            .children();
+
+    // change ingredient.property 'id'
+    // id="ingredient'newIdOfIngredient'.ingredientPropertyName"
+    inputElementToChange.attr(
+        'id',
+        'ingredients' + newIdOfIngredient + '.' + ingredientPropertyName
+    );
+    // change ingredient.property 'name'
+    // name="ingredient['newIdOfIngredient'].ingredientPropertyName"
+    inputElementToChange.attr(
+        'name',
+        'ingredients[' + newIdOfIngredient + '].' + ingredientPropertyName
+    );
+
+    // remove values, so that inputs are blank
+    inputElementToChange.removeAttr('value');
+
+}
+
+
+// function adding new ingredient
+$("#add-another-ingredient-button").click(function () {
+    // can add prevent default so that
+    // no need to write button type=button
+    // in html5
+    event.preventDefault();
+
+    // define <div> with ingredient that we will be appending to
+    var divWithLastIngredient = $(".ingredient-row").last();
+
+    // get id as String of the last step to generate new one
+    // it is kind of wacky .. but well I'm JS newbie, so
+    var idAsString =
+        divWithLastIngredient
+        .children('div').first()
+        .children('p')
+        .children()
+        .attr('id')
+        .split('.')[0]
+        .split('ingredients')[1];
+
+    // if id is not a number we set id to zero
+    // otherwise we parse it
+    var id = 0;
+    if (!isNaN(idAsString)) {
+        id = parseInt(idAsString);
+    }
+
+    // set new id
+    var newIngredientId = id + 1;
+
+    // clone div and add new one
+    var cloneOfTheLastDivWithIngredientRow =
+        divWithLastIngredient.clone();
+
+    // add clone to the end
+    divWithLastIngredient.after(
+        cloneOfTheLastDivWithIngredientRow
+    );
+
+    // change ingredient.item select
+    changeSelectItemElement(
+        cloneOfTheLastDivWithIngredientRow,
+        newIngredientId
+    );
+
+    // change ingredient.quantity
+    changeIngredientInputElement(
+        cloneOfTheLastDivWithIngredientRow,
+        newIngredientId,
+        1,
+        'quantity'
+    );
+
+    // change ingredient.condition
+    changeIngredientInputElement(
+        cloneOfTheLastDivWithIngredientRow,
+        newIngredientId,
+        2,
+        'condition'
     );
 
 });
