882f78bc93be463d6d41eaea7a1d3af7be43cdeb
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-09-24 09:56:50 +0200

    optimized imports

--- src/main/java/com/techdegree/web/controller/RecipeController.java
+++ src/main/java/com/techdegree/web/controller/RecipeController.java
@@ -1,6 +1,5 @@
 package com.techdegree.web.controller;
 
-import com.techdegree.model.Ingredient;
 import com.techdegree.model.Recipe;
 import com.techdegree.model.RecipeCategory;
 import com.techdegree.service.IngredientService;
@@ -17,8 +16,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/recipes")
