cccd60e2db679739e09bdf530c17ad426a84303b
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-09-25 20:09:36 +0200

    added WebConstantsClass with RECIPES_HOME_PAGE constant, to use it just in case in all test methods as one of very common page. Changed code in newly defined IndexRedirectController method

---
+++ src/main/java/com/techdegree/web/WebConstants.java
@@ -0,0 +1,5 @@
+package com.techdegree.web;
+
+public final class WebConstants {
+    public static final String RECIPES_HOME_PAGE = "/recipes/";
+}
--- src/main/java/com/techdegree/web/controller/IndexRedirectController.java
+++ src/main/java/com/techdegree/web/controller/IndexRedirectController.java
@@ -3,6 +3,8 @@ package com.techdegree.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import static com.techdegree.web.WebConstants.RECIPES_HOME_PAGE;
+
 // class with redirects from some pages
 // to home page
 @Controller
@@ -10,11 +12,11 @@ public class IndexRedirectController {
 
     @RequestMapping("/")
     public String root() {
-        return "redirect:/recipes/";
+        return "redirect:" + RECIPES_HOME_PAGE;
     }
 
     @RequestMapping("/recipes")
     public String redirectFromRecipesWithoutSlash() {
-        return "redirect:/recipes/";
+        return "redirect:" + RECIPES_HOME_PAGE;
     }
 }
