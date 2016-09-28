c3a893ff1f48caa705a2258de4343e0f8ac1e742
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-09-28 21:17:42 +0200

    added updating recipe test that checks that owner was not created but rather taken from recipe, that recipe service was called 2 times and user was added to favorites

--- src/test/java/com/techdegree/service/RecipeServiceTest.java
+++ src/test/java/com/techdegree/service/RecipeServiceTest.java
@@ -8,9 +8,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.hamcrest.Matchers.is;
+import java.util.Collections;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.*;
-import static org.junit.Assert.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RecipeServiceTest {
@@ -81,6 +84,87 @@ public class RecipeServiceTest {
         // verify mock interactions
         verify(itemService).findOne(1L);
         verify(ownerService).save(any(Owner.class));
+        verify(recipeDao).save(any(Recipe.class));
+    }
+
+    @Test
+    public void
+    updatingRecipeDoesNotInvokeOwnerServiceAndItemServicesAndSetsRecipeItems()
+            throws Exception {
+        // Arrange: create test Recipe to be saved
+        // with one ingredient
+        Recipe testRecipe =
+                new Recipe();
+        testRecipe.setId(1L);
+        Ingredient ingredient = new Ingredient(
+                new Item(), "", ""
+        );
+        testRecipe.addIngredient(ingredient);
+
+        // Arrange testOwner that we'll check
+        Owner testOwner = new Owner(new User());
+        User testUser = new User();
+        // Arrange set recipe owner and favorites
+        testRecipe.setOwner(
+                testOwner
+        );
+        testRecipe.setFavoriteUsers(
+                Collections.singletonList(
+                        testUser
+                )
+        );
+
+        // Arrange:
+        // make itemService return item when called
+        when(itemService.findOne(1L)).thenReturn(
+                new Item()
+        );
+        // Arrange:
+        // make recipeService findOne return test
+        // recipe upon 2 calls
+        when(recipeDao.findOne(1L)).thenReturn(
+                testRecipe
+        );
+        when(recipeDao.findOne(1L)).thenReturn(
+                testRecipe
+        );
+        // Arrange:
+        // when recipeDao.save will be called
+        // we save recipe that was passed
+        doAnswer(
+                invocation -> {
+                    Recipe r = invocation.getArgumentAt(0, Recipe.class);
+                    return r;
+                }
+        ).when(recipeDao).save(any(Recipe.class));
+
+        // Act:
+        // when we call recipeService.save method
+        Recipe savedRecipe = recipeService.save(
+                testRecipe,
+                new User()
+        );
+
+        // Assert that: recipe.owner was set to testOwner
+        assertThat(
+                savedRecipe.getOwner(),
+                is(testOwner)
+        );
+        // Assert that: recipe.favorites has size 1,
+        // and contains testUser
+        assertThat(
+                savedRecipe.getFavoriteUsers(),
+                iterableWithSize(1)
+        );
+        assertThat(
+                savedRecipe.getFavoriteUsers(),
+                containsInAnyOrder(testUser)
+        );
+
+        // verify mock interactions
+        verify(itemService).findOne(any());
+        verifyNoMoreInteractions(ownerService);
+        verify(recipeDao, times(2)).findOne(1L);
         verify(recipeDao).save(any(Recipe.class));
     }
 }
