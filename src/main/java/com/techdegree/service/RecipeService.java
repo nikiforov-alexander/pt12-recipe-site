68ab3d5a949ca54f5f39f4dbbfb890481fd8532e
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-09-28 15:02:22 +0200

    changed the signature of `save` method now it has second argument, user. Also now in this method we set Items by id not in controller, we set owner, from ownerService, or creating a new one. And we also set recipe's favorites, if recipe is being updated.

--- src/main/java/com/techdegree/service/RecipeService.java
+++ src/main/java/com/techdegree/service/RecipeService.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface RecipeService {
     List<Recipe> findAll();
     Recipe findOne(Long id);
-    Recipe save(Recipe recipe);
+    Recipe save(Recipe recipe, User user);
     void delete(Recipe recipe);
     List<Recipe> findFavoriteRecipesForUser(User user);
 }
