43a0b54eab14a35325c3cf95b1241ffd6fd26277
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-10-01 16:18:42 +0200

    added `equals` and `hashCode` with `id`, `username` and `name` so that we can use that in tests to check equality

--- src/main/java/com/techdegree/model/User.java
+++ src/main/java/com/techdegree/model/User.java
@@ -177,4 +177,28 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         return enabled;
     }
+
+    // equals and hashcode
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+        if (name != null ? !name.equals(user.name) : user.name != null)
+            return false;
+        return username != null ? username.equals(user.username) : user.username == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        return result;
+    }
 }
