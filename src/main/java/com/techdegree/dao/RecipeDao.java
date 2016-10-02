80c8e40a33569ca649941abb9b589d2eb260477f
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-10-02 17:40:14 +0200

    added Spring Query method `findByRecipeCategory`

--- src/main/java/com/techdegree/dao/RecipeDao.java
+++ src/main/java/com/techdegree/dao/RecipeDao.java
@@ -1,8 +1,11 @@
 package com.techdegree.dao;
 
 import com.techdegree.model.Recipe;
+import com.techdegree.model.RecipeCategory;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 // second interface that is extended
 // allows us to use methods implemented in
@@ -16,4 +19,5 @@ public interface RecipeDao extends
         CrudRepository<Recipe, Long>,
         FavoriteRecipesDao {
 
+    List<Recipe> findByRecipeCategory(RecipeCategory recipeCategory);
 }
