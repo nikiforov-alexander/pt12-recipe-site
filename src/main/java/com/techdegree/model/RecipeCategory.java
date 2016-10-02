968e8ec1875bf46032626f11c150a04470275b3b
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-10-02 17:17:58 +0200

    added new method returning RecipeCategory from `htmlName` passed. Also added new field `htmlName` with getter and inserted it into constructor

--- src/main/java/com/techdegree/model/RecipeCategory.java
+++ src/main/java/com/techdegree/model/RecipeCategory.java
@@ -1,22 +1,25 @@
 package com.techdegree.model;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 // This will be first iteration of RecipeCategory
 // it will be enum and values will be pre-defined
 // I think it is a good idea to extract RecipeCategory
 // from here and be able to add new one
 public enum RecipeCategory {
-    NONE("Other", "category other"),
-    BREAKFAST("Breakfast", "category breakfast"),
-    LUNCH("Lunch", "category lunch"),
-    DINNER("Dinner", "category dinner"),
-    DESSERT("Dessert", "category dessert");
+    NONE("Other", "category other", "other"),
+    BREAKFAST("Breakfast", "category breakfast", "breakfast"),
+    LUNCH("Lunch", "category lunch", "lunch"),
+    DINNER("Dinner", "category dinner", "dinner"),
+    DESSERT("Dessert", "category dessert", "dinner");
 
     // fields
 
     private final String name;
+
+    // this name will be used in query to sort
+    // by category on index page
+    private final String htmlName;
     // this field will be the name of respectful CSS
     // style class, that will let us change style
     // according to selected option
@@ -33,11 +36,19 @@ public enum RecipeCategory {
         return styleClass;
     }
 
+    public String getHtmlName() {
+        return htmlName;
+    }
+
     // constructors
 
-    RecipeCategory(String name, String styleClass) {
+    RecipeCategory(
+            String name,
+            String styleClass,
+            String htmlName) {
         this.name = name;
         this.styleClass = styleClass;
+        this.htmlName = htmlName;
     }
 
     // TODO: fix this ugly code: for now it throws UnsupportedOpException
@@ -50,5 +61,21 @@ public enum RecipeCategory {
                     RecipeCategory.DINNER
                );
        return categoriesWithoutNone;
+    }
+
+    public static RecipeCategory
+    getRecipeCategoryWithHtmlName(String htmlName) {
+        Map<String, RecipeCategory> mapWithHtmlNames =
+                new HashMap<>();
+        for (RecipeCategory recipeCategory : values()) {
+            mapWithHtmlNames.put(
+                    recipeCategory.getHtmlName(),
+                    recipeCategory
+            );
+        }
+        return mapWithHtmlNames.getOrDefault(
+                        htmlName,
+                        RecipeCategory.NONE
+        );
     }
 }
