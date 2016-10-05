4b05bb2014a492bf5ca052d986441c2d657a0881
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-10-05 22:02:43 +0200

    added tests checking registering new user method in CustomUserDetailsServiceTest

--- src/test/java/com/techdegree/service/CustomUserDetailsServiceTest.java
+++ src/test/java/com/techdegree/service/CustomUserDetailsServiceTest.java
@@ -2,6 +2,9 @@ package com.techdegree.service;
 
 import com.techdegree.dao.RoleDao;
 import com.techdegree.dao.UserDao;
+import com.techdegree.dto.UserDto;
+import com.techdegree.exception.UserAlreadyExistsException;
+import com.techdegree.model.Role;
 import com.techdegree.model.User;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,6 +14,8 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -71,5 +76,92 @@ public class CustomUserDetailsServiceTest {
         // Assert:
         // Then UsernameNotFoundException should be
         // thrown
+    }
+
+    @Test
+    public void userCanBeRegisteredIfUsernameIsFreeAndUserDtoPropertiesArePassedToUser()
+            throws Exception {
+        // Arrange: userDto passed from Controller
+        UserDto userDto = new UserDto();
+        userDto.setName("name");
+        userDto.setUsername("username");
+        userDto.setPassword("password");
+
+        // Arrange: when findByUsername will be called, we
+        // return null, to know that such user does not exist
+        when(userDao.findByUsername(anyString())).thenReturn(
+                null
+        );
+
+        // Arrange:
+        // When new user is saved we pass to it
+        // userDto object above, so that we can later
+        // check whether data from userDto was passed
+        when(userDao.save(any(User.class)))
+                .thenReturn(
+                        new User(userDto, any(Role.class)
+                        )
+                );
+
+        // Act:
+        // When registerNewUser is called
+        User savedUser =
+                userService.registerNewUser(userDto);
+
+        // Then saved user's name and username should be
+        // equal to the ones of userDto passed as argument
+        assertThat(
+                savedUser,
+                hasProperty("name",
+                        equalTo(userDto.getName()))
+        );
+        assertThat(
+                savedUser,
+                hasProperty("username",
+                        equalTo(userDto.getUsername()))
+        );
+        // This test does not work ... hopefully it is
+        // because this PASSWORD_ENCODER works very securely...
+        // TODO: ask why PASSWORD_ENCODER does not work ?
+//        assertThat(
+//                savedUser,
+//                hasProperty("password",
+//                        equalTo(
+//                                User.PASSWORD_ENCODER.encode(
+//                                        userDto.getPassword()
+//                                )
+//                        )
+//                )
+//        );
+        //
+        // verify mock interactions
+        verify(userDao).findByUsername(anyString());
+        verify(userDao).save(any(User.class));
+    }
+
+    @Test(expected = UserAlreadyExistsException.class)
+    public void whenUsernamePassedToRegisterNewUserAlreadyExistsExceptionShouldBeThrown()
+            throws Exception {
+        // Arrange: userDto passed from Controller
+        UserDto userDto = new UserDto();
+        userDto.setName("name");
+        userDto.setUsername("username");
+        userDto.setPassword("password");
+
+        // Arrange: when findByUsername will be called, we
+        // return some user
+        // NOTE: does not work with any(User.class) ... ?
+        // I wonder why ?
+        // TODO: figure out what does any() mean ? - null ?
+        when(userDao.findByUsername(anyString())).thenReturn(
+                new User()
+        );
+
+        // Act:
+        // When user.registerNewUser will be called
+        userService.registerNewUser(userDto);
+
+        // Assert:
+        // Exception should be thrown
     }
 }
