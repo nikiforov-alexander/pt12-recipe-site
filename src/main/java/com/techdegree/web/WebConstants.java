c3b059e8aee4642df1247b6aa1dcd3a09524d9de
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-10-07 14:08:12 +0200

    added page URI-s and thymeleaf template names as constants

--- src/main/java/com/techdegree/web/WebConstants.java
+++ src/main/java/com/techdegree/web/WebConstants.java
@@ -1,5 +1,19 @@
 package com.techdegree.web;
 
 public final class WebConstants {
+
+    // URI-s
+
     public static final String RECIPES_HOME_PAGE = "/recipes/";
+    public static final String SIGN_UP_PAGE = "/sign-up";
+    public static final String LOGIN_PAGE = "/login/";
+
+
+    // Thymeleaf templates
+
+    public static final String INDEX_TEMPLATE = "index";
+    public static final String DETAIL_TEMPLATE = "detail";
+    public static final String LOGIN_TEMPLATE = "login";
+    public static final String PROFILE_TEMPLATE = "profile";
+    public static final String SIGN_UP_TEMPLATE = "signup";
 }
