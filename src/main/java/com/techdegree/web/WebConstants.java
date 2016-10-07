718b89dc32a7b578f455bb036376be7c26bba1f9
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-10-07 16:30:45 +0200

    changed LOGIN_PAGE constant back to one without slash, and also added new `BINDING_RESULT_MAIN_PACKAGE` constant used a lot in redirect attributes everywhere

--- src/main/java/com/techdegree/web/WebConstants.java
+++ src/main/java/com/techdegree/web/WebConstants.java
@@ -6,7 +6,7 @@ public final class WebConstants {
 
     public static final String RECIPES_HOME_PAGE = "/recipes/";
     public static final String SIGN_UP_PAGE = "/sign-up";
-    public static final String LOGIN_PAGE = "/login/";
+    public static final String LOGIN_PAGE = "/login";
 
 
     // Thymeleaf templates
@@ -16,4 +16,9 @@ public final class WebConstants {
     public static final String LOGIN_TEMPLATE = "login";
     public static final String PROFILE_TEMPLATE = "profile";
     public static final String SIGN_UP_TEMPLATE = "signup";
+
+    // Binding result full package name
+
+    public static final String BINDING_RESULT_PACKAGE_NAME =
+            "org.springframework.validation.BindingResult";
 }
