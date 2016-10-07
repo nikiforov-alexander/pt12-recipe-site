c8f8f985d6efa1639ba3fe628fd5241868d03033
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-10-07 13:02:48 +0200

    added equals and `hashCode` methods in UserDto, for comparison in tests with all fields included

--- src/main/java/com/techdegree/dto/UserDto.java
+++ src/main/java/com/techdegree/dto/UserDto.java
@@ -65,4 +65,35 @@ public class UserDto {
     public UserDto() {
 
     }
+
+    // overrides
+
+    // equals and hashcode
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserDto userDto = (UserDto) o;
+
+        if (name != null ? !name.equals(userDto.name) : userDto.name != null)
+            return false;
+        if (username != null ? !username.equals(userDto.username) : userDto.username != null)
+            return false;
+        if (password != null ? !password.equals(userDto.password) : userDto.password != null)
+            return false;
+        return matchingPassword != null ? matchingPassword.equals(userDto.matchingPassword) : userDto.matchingPassword == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (matchingPassword != null ? matchingPassword.hashCode() : 0);
+        return result;
+    }
 }
