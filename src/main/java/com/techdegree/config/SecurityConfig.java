899a3e2f0fb65bec72c70dd636d400822f888da3
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-10-10 15:25:30 +0200

    added sign-up page as exception, so that when app starts, we can access only two pages: sign-up and login. Also changed success redirect page back to home page, and changed access of CustomUserSecurityDetails through interface, just in case

--- src/main/java/com/techdegree/config/SecurityConfig.java
+++ src/main/java/com/techdegree/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.techdegree.config;
 
 import com.techdegree.model.User;
 import com.techdegree.service.CustomUserDetailsService;
+import com.techdegree.service.UserService;
 import com.techdegree.web.FlashMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -21,6 +22,7 @@ import org.springframework.security.web.authentication.AuthenticationFailureHand
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import static com.techdegree.web.WebConstants.LOGIN_PAGE;
+import static com.techdegree.web.WebConstants.RECIPES_HOME_PAGE;
 import static com.techdegree.web.WebConstants.SIGN_UP_PAGE;
 
 @Configuration
@@ -29,7 +31,7 @@ import static com.techdegree.web.WebConstants.SIGN_UP_PAGE;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
-    private CustomUserDetailsService userService;
+    private UserService userService;
 
     // figure out what does this mean
     @Autowired
@@ -56,6 +58,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                // here we permit sign up page to be open
+                // as well as login page
+                .antMatchers(SIGN_UP_PAGE).permitAll()
                 .anyRequest()
                 // it infers "ROLE_USER"
                 .hasRole("USER")
@@ -77,7 +82,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // later will change that back to referrer maybe ...
     public AuthenticationSuccessHandler loginSuccessHandler() {
         return (request, response, authentication) ->
-                response.sendRedirect(SIGN_UP_PAGE);
+                response.sendRedirect(RECIPES_HOME_PAGE);
     }
 
     // in case of failure we redirect back to "/login" with error
