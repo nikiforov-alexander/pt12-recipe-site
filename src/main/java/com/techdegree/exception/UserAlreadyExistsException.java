0287d9a5a7a0a629c9f21630b06d9bb594a341d9
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-10-15 22:05:47 +0200

    added TODO to refactor UserAlreadyExistsException later

--- src/main/java/com/techdegree/exception/UserAlreadyExistsException.java
+++ src/main/java/com/techdegree/exception/UserAlreadyExistsException.java
@@ -1,5 +1,6 @@
 package com.techdegree.exception;
 
+// TODO : refactor this to be used with @ControllerAdvice and remove try catch
 public class UserAlreadyExistsException extends RuntimeException {
     public UserAlreadyExistsException() {
         super("User with this username already exists");
