db0def4ca038eba0d75019c4b4f19c8d64027e26
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-10-15 21:28:31 +0200

    added `@HandleBeforeSave` annotated method checks whether user is owner or admin before updating recipe

--- src/main/java/com/techdegree/handler/RecipeEventHandler.java
+++ src/main/java/com/techdegree/handler/RecipeEventHandler.java
@@ -1,11 +1,15 @@
 package com.techdegree.handler;
 
 import com.techdegree.dao.RecipeDao;
+import com.techdegree.exception.CustomAccessDeniedException;
+import com.techdegree.exception.UserAlreadyExistsException;
 import com.techdegree.model.Recipe;
 import com.techdegree.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
+import org.springframework.data.rest.core.annotation.HandleBeforeSave;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -24,4 +28,24 @@ public class RecipeEventHandler {
                 .getPrincipal();
         recipe.setOwner(user);
     }
+
+    @HandleBeforeSave
+    public void checkIfOwnerOrAdminIsEditing(Recipe recipe)
+    throws UserAlreadyExistsException {
+        User user = (User)
+                SecurityContextHolder
+                        .getContext()
+                        .getAuthentication()
+                        .getPrincipal();
+        // if user is not owner or user is not admin
+        // we throw access denied exception
+        if (!recipe.getOwner().equals(user) &&
+                !user.getRole().getName().equals("ROLE_ADMIN")
+                ) {
+            throw new CustomAccessDeniedException(
+                    "Only user can edit Recipe"
+            );
+        }
+    }
+
 }
