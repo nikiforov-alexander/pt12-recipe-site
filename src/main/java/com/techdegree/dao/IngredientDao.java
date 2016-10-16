0e6c83e4a10906025b1dacaf9c29c1f7873567eb
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-10-16 19:55:43 +0200

    added `@PreAuthorize` annotation not allowing non-owner, non-admin user to save ingredient

--- src/main/java/com/techdegree/dao/IngredientDao.java
+++ src/main/java/com/techdegree/dao/IngredientDao.java
@@ -2,9 +2,14 @@ package com.techdegree.dao;
 
 import com.techdegree.model.Ingredient;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IngredientDao extends CrudRepository<Ingredient, Long> {
-
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN') or " +
+            "#ingredient.recipe.owner == authentication.principal")
+    Ingredient save(@Param("ingredient") Ingredient ingredient);
 }
