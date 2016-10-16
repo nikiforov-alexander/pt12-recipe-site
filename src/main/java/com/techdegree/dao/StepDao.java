e62eab725d9993d448cae07e99ea126229413848
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-10-16 16:17:05 +0200

    added @PreAuthorize to `save` method in StepDao, so that before Step is saved/updated it is checked whether Step.recipe is owned by currently logged user or user is admin

--- src/main/java/com/techdegree/dao/StepDao.java
+++ src/main/java/com/techdegree/dao/StepDao.java
@@ -2,9 +2,17 @@ package com.techdegree.dao;
 
 import com.techdegree.model.Step;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StepDao extends CrudRepository<Step, Long> {
-
+    // Step can be saved only if user is admin
+    // or step.recipe.owner is currently logged
+    // in user
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN') or " +
+        "#step.recipe.owner == authentication.principal")
+    Step save(@Param("step") Step step);
 }
