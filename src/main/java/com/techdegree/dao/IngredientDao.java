aa34f9e3bd0dd1c87b27bebd43dd8c2b7f3422ff
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-10-19 10:18:16 +0200

    added @PreAuthorize annotation to `delete` method in IngredientDao, the same as in `save` so that only owner or admin can delete ingredient

--- src/main/java/com/techdegree/dao/IngredientDao.java
+++ src/main/java/com/techdegree/dao/IngredientDao.java
@@ -6,8 +6,17 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
+// TODO : test all these methods and add other delete and save methods
 @Repository
 public interface IngredientDao extends CrudRepository<Ingredient, Long> {
+    // ingredient can be saved and/or deleted in REST API
+    // only if user is owner of recipe, or admin
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN') or " +
+            "#ingredient.recipe.owner == authentication.principal")
+    void delete(@Param("ingredient") Ingredient ingredient);
+
     @Override
     @PreAuthorize("hasRole('ROLE_ADMIN') or " +
             "#ingredient.recipe.owner == authentication.principal")
