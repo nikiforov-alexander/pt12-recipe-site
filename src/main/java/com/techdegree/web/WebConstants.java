07c0eea1c49ce0dd9ae87764397bd0801088ea2d
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-11-01 12:58:41 +0100

    added new Web constants
    to make sure we cover all the application

--- src/main/java/com/techdegree/web/WebConstants.java
+++ src/main/java/com/techdegree/web/WebConstants.java
@@ -5,13 +5,26 @@ public final class WebConstants {
     // URI-s
 
     public static final String RECIPES_HOME_PAGE = "/recipes/";
+
+    public static final String POST_SAVE_ADDRESS = RECIPES_HOME_PAGE + "save";
+
     public static final String SIGN_UP_PAGE = "/sign-up";
     public static final String LOGIN_PAGE = "/login";
     public static final String PROFILE_PAGE = "/profile";
+    public static final String ADD_NEW_PAGE = RECIPES_HOME_PAGE + "add-new";
+
     public static final String UPDATE_FAVORITES_PAGE_PREFIX =
             "/update-favorite-status-of-recipe";
     public static final String EDIT_RECIPE_PAGE_PREFIX =
             "/edit";
+    public static final String DELETE_RECIPE_PAGE_PREFIX =
+            "/delete";
+
+    public static String getDeleteRecipePageWithId(String id) {
+        return RECIPES_HOME_PAGE +
+                DELETE_RECIPE_PAGE_PREFIX +
+                "/" + id;
+    }
 
     public static String
     getEditRecipePageWithId(String id) {
@@ -29,7 +42,6 @@ public final class WebConstants {
 
     public static final String RECIPES_REST_PAGE = "/recipes";
     public static final String INGREDIENTS_REST_PAGE = "/ingredients";
-    public static final String STEPS_REST_PAGE = "/steps";
     public static final String ITEMS_REST_PAGE = "/items";
 
 
@@ -40,6 +52,7 @@ public final class WebConstants {
     public static final String LOGIN_TEMPLATE = "login";
     public static final String PROFILE_TEMPLATE = "profile";
     public static final String SIGN_UP_TEMPLATE = "signup";
+    public static final String EDIT_TEMPLATE = "edit";
 
     // Binding result full package name
 
